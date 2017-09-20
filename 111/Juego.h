a47fea2b03162276994145a84916ea6fc06c9288
anibal valenzuela <anibal.valenzuela@ucsp.edu.pe>
2017-09-20 09:15:21 -0500

    1.0

---
+++ 111/Juego.h
@@ -0,0 +1,143 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include "Jugador.h"
+#include "CNTree.h"
+#include <stdlib.h>
+
+
+class Juego
+{
+    public:
+        int **tablero;
+        Juego()
+        {
+            tablero = new int*[8];
+            for(int i=0;i<8;i++)
+                tablero[i] = new int[8];
+
+            //inicializando en ceros
+            for(int i =0;i<8;i++)
+                for(int j=0;j<8;j++)
+                    tablero[i][j] = 0;
+            //posicionando fichas
+            tablero[0][1] = 1;
+            tablero[0][3] = 1;            tablero[0][5] = 1;            tablero[0][7] = 1;            tablero[1][0] = 1;            tablero[1][2] = 1;            tablero[1][4] = 1;            tablero[1][6] = 1;            tablero[2][1] = 1;            tablero[2][3] = 1;            tablero[2][5] = 1;            tablero[2][7] = 1;
+            tablero[5][0] = 2;            tablero[5][2] = 2;            tablero[5][4] = 2;            tablero[5][6] = 2;            tablero[6][1] = 2;            tablero[6][3] = 2;            tablero[6][5] = 2;            tablero[6][7] = 2;            tablero[7][0] = 2;            tablero[7][2] = 2;            tablero[7][4] = 2;            tablero[7][6] = 2;
+        }
+
+        void imprimir_tablero()
+        {
+            cout << "   0 1 2 3 4 5 6 7\n\n";
+            for(int i = 0;i<8;i++)
+            {
+                cout << i << "  ";
+                for(int j=0;j<8;j++)
+                {
+                    cout << tablero[i][j] << " ";
+                }
+                cout << "\n";
+            }
+            cout << endl << endl;
+        }
+
+        void jugar()
+        {
+            CNTree *ia;
+            int ficha_jugador,ficha_ia,dificultad;
+            bool turno;
+            cout << "escoja la ficha 1 o 2:"  << endl;
+            cin >> ficha_jugador;
+
+            if(ficha_jugador > 2 || ficha_jugador < 1)
+            {
+                cout << "se asigno ficha 1" << endl;
+                ficha_jugador = 1;
+                ficha_ia =2;
+            }
+            else if(ficha_jugador == 1)
+                ficha_ia = 2;
+            else
+                ficha_ia = 1;
+            cout  <<  "ingrese el dificultad 0-6: ";
+            cin >> dificultad;
+            if (dificultad <0 || dificultad > 6)
+            {
+                cout << "se asigno dificultad en 6" << endl;
+                dificultad = 6;
+            }
+
+            Jugador jugador1(ficha_jugador);
+
+            if(ficha_jugador == 1)
+                turno =true;
+            else
+                turno =false;
+
+            ///loop
+            while(victoria_derrota())
+            {
+                system("cls");
+                imprimir_tablero();
+                cout << endl;
+
+                if(turno)
+                {
+                    jugador1.calcular_mov(tablero);
+                    while(!jugador1.jugada(tablero))
+                    {
+                        cout << "valores incorectos\n";
+
+                    }
+                    turno =false;
+
+                }
+                else
+                {
+                    ia = new CNTree(ia->copiar_matriz(tablero),dificultad,ficha_ia);
+                    ia->natalidad(ia->c_root);
+                    //prueba1.imprimirComportamiento(prueba1.c_root);
+                    int neo = ia->camino(ia->c_root);
+
+                    tablero = ia->c_root->c_hijos[neo]->tablero_estado;
+                    delete ia;
+                    turno =true;
+                }
+
+                system("Pause");
+            }
+        }
+
+        bool victoria_derrota()
+        {
+            bool p1 = 0; // fichas 1
+            bool p2 = 0; // fichas 2
+
+            for(int i = 0;i<8;i++)
+                for(int j=0;j<8;j++)
+                {
+                    if(tablero[i][j] == 1)
+                        p1 =  true;
+                    if(tablero[i][j] == 2)
+                        p2 = true;
+                }
+            if(!p1)
+            {
+                cout << "fichas 2 ha ganado" << endl;
+                return false;
+            }
+            if(!p2)
+            {
+                cout << "fichas 1 ha ganado" << endl;
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+        virtual ~Juego() {}
+};
+
+#endif // JUEGO_H
---
+++ 111/Jugador.h
@@ -0,0 +1,167 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+#include <iostream>
+
+using namespace std;
+
+class Jugador
+{
+    public:
+        int m_player;
+        Jugador(int f) {m_player = f; }
+        virtual ~Jugador() {}
+
+        bool jugada(int ** &tablero_estado)
+        {
+            int m_y,m_x,m_y2,m_x2;
+            cout << "ingrese las coordenadas de la ficha a mover: (y,x) ";
+            cin >> m_y >> m_x;
+
+            if(m_y < 0 || m_y > 7|| m_x < 0 || m_x > 7) return false;
+
+            cout << "ingrese las coordenadas de movimiento de su ficha: (y x) ";
+            cin >> m_y2 >> m_x2;
+
+            if(m_y2 < 0 || m_y2 > 7 || m_x2 < 0 || m_x2 > 7) return false;
+
+            if(m_x2-2 == m_x && aux_calc(m_y,2) == m_y2 && comer_der(m_y,m_x,tablero_estado))
+            {
+                tablero_estado[m_y][m_x] = 0;
+                tablero_estado[aux_calc(m_y,1)][m_x+1] = 0;
+                tablero_estado[m_y2][m_x+2] = m_player;
+                return true;
+            }
+            else if(m_x2+2 == m_x && aux_calc(m_y,2) == m_y2 && comer_izq(m_y,m_x,tablero_estado))
+            {
+                tablero_estado[m_y][m_x] = 0;
+                tablero_estado[aux_calc(m_y,1)][m_x-1] = 0;
+                tablero_estado[m_y2][m_x2] = m_player;
+                return true;
+            }
+            else if(m_x2-1 == m_x && aux_calc(m_y,1) == m_y2 && mover_der(m_y,m_x,tablero_estado))
+            {
+                tablero_estado[m_y][m_x] = 0;
+                tablero_estado[m_y2][m_x2] = m_player;
+                return true;
+            }
+            else if(m_x2+1 == m_x && aux_calc(m_y,1) == m_y2 && mover_izq(m_y,m_x,tablero_estado))
+            {
+                tablero_estado[m_y][m_x] = 0;
+                tablero_estado[m_y2][m_x2] = m_player;
+                return true;
+            }
+
+
+            return false;
+        }
+
+
+
+        void calcular_mov(int **tablero_estado)
+        {
+            cout << "movimientos:\n";
+            bool comer_primero;
+            for(int i = 0;i<8;i++)
+                for(int j=0;j<8;j++)
+                {
+                    if(tablero_estado[i][j] == m_player)
+                    {
+                        comer_primero = false;
+                        if(comer_der(i,j,tablero_estado))
+                        {
+                            cout << "ficha: ("  << i << "," << j << ") -> (" << aux_calc(i,2) <<"," <<j-2 << ")" << endl;
+                            comer_primero =true;
+                        }
+
+                        if(comer_der(i,j,tablero_estado))
+                        {
+                            cout << "ficha: ("  << i << "," << j << ") -> (" << aux_calc(i,2) <<"," <<j+2 << ")" << endl;
+                            comer_primero = true;
+                        }
+                        if(mover_izq(i,j,tablero_estado) && !comer_primero)
+                        {
+                            cout << "ficha: ("  << i << "," << j << ") -> (" << aux_calc(i,1) <<"," <<j-1 << ")" << endl;
+                        }
+
+                        if(mover_der(i,j,tablero_estado) && !comer_primero)
+                        {
+                            cout << "ficha: ("  << i << "," << j << ") -> (" << aux_calc(i,1) <<"," <<j+1 << ")" << endl;
+                        }
+                    }
+                }
+        }
+
+
+        bool mover_izq(int y,int x,int **matriz)
+        {
+            int new_y =  aux_calc(y,1);
+            if(x-1 >= 0 && aux_calc2(new_y) && matriz[new_y][x-1] == 0)
+            {
+                /*
+                matriz[y][x] = 0;
+                matriz[new_y][x-1] = m_player;
+                */
+                return 1;
+            }
+            return 0;
+        }
+
+        bool mover_der(int y, int x,int **matriz)
+        {
+            int new_y =  aux_calc(y,1);
+            if(x+1 < 8 && aux_calc2(new_y) && matriz[new_y][x+1] == 0)
+            {
+                /*
+                matriz[y][x] = 0;
+                matriz[new_y][x+1] = m_player;
+                */
+                return 1;
+            }
+            return 0;
+        }
+
+        bool comer_izq(int y,int x,int **matriz)
+        {
+            if(x-2 >= 0 && aux_calc2(aux_calc(y,2)) && matriz[aux_calc(y,1)][x-1] !=0 && matriz[aux_calc(y,1)][x-1] !=m_player )
+            {
+                /*
+                matriz[y][x] = 0;
+                matriz[aux_calc(y,1)][x-1] = 0;
+                matriz[aux_calc(y,2)][x-2] = m_player;
+                */
+                return 1;
+            }
+            return 0;
+        }
+
+        bool comer_der(int y, int x,int **matriz)
+        {
+            if(x+2 < 8 && aux_calc2(aux_calc(y,2)) && matriz[aux_calc(y,1)][x+1] !=0 && matriz[aux_calc(y,1)][x+1] !=m_player )
+            {
+                /*
+                matriz[y][x] = 0;
+                matriz[aux_calc(y,1)][x+1] = 0;
+                matriz[aux_calc(y,2)][x+2] = m_player;
+                */
+                return 1;
+            }
+            return 0;
+        }
+
+        int aux_calc(int y,int sum_res)
+        {
+            if(m_player==1)
+                return y+sum_res;
+            return y-sum_res;
+        }
+        bool aux_calc2(int y)
+        {
+            if(m_player==1)
+                return y<8;
+            return y>=0;
+        }
+
+    private:
+};
+
+#endif // JUGADOR_H
